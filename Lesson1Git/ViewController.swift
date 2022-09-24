c87b72d001409b6b1763c0cf0f7234fab27e9cf6
Bogatchuk Roman <bogatchukroma2013@gmail.com>
2022-09-24 15:36:35 +0300

    added traffic loght logic

--- Lesson1Git/ViewController.swift
+++ Lesson1Git/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroungLightView: UIView!
+    @IBOutlet var lightViews: [UIView]!
+    @IBOutlet weak var switchButton: UIButton!
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroungLightView.layer.cornerRadius = 10
+        switchButton.layer.cornerRadius = 10
+        
+        for view in lightViews {
+            view.layer.cornerRadius = 30
+            view.alpha = 0.5
+        }
     }
 
-
+    @IBAction func switchButtonClicked(_ sender: UIButton) {
+        
+        switchButton.setTitle("NEXT", for: .normal)
+        for view in lightViews {
+            view.alpha = 0.5
+        }
+        lightViews[index].alpha = 1
+        
+        if index >= 2 {
+            index = 0
+        } else {
+            index += 1
+        }
+        
+    }
+    
 }
 
